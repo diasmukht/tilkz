45f60c6dc51ab9131efca659c533ad4475138686
diasmukht <diaspias05@gmail.com>
2025-05-08 22:26:53 +0600

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('courses/', views.course_list, name='course_list'),
     path('courses/<slug:course_slug>/lesson/<int:lesson_order>/', views.lesson_detail, name='lesson_detail'),
+    path('add-courses/', views.add_courses_view, name='add_courses'),  # ✅ жаңа бағыт
 ]
