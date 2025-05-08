330606255d5ed6b6b569534eb19c349f13e4a593
diasmukht <diaspias05@gmail.com>
2025-05-08 22:07:33 +0600

    admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Course, Lesson, Question, Answer, PassedLesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'language', 'status')
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order')
+    ordering = ('course', 'order')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'lesson')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+
+
+@admin.register(PassedLesson)
+class PassedLessonAdmin(admin.ModelAdmin):
+    list_display = ('user', 'lesson', 'passed')
